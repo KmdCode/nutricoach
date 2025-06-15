b708a3fcd627b52537035b95f654833fb42dc62f
karabomodise260@gmail.com <karabomodise260@gmail.com>
2025-06-15 12:36:06 +0200

    Code restructured to fix deployment errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import NavBar from "@/components/Navbar";
+import NavBar from "@/components/navbar/Navbar";
 import styles from "./page.module.css";
 
 const LandingPage = () => {
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./styles/NavBar.module.css";
+import styles from "./Navbar.module.css"
 import React from "react";
 
 const NavBar: React.FC = () => {
